bebd214213232dc50b9d9be90127688d984b0100
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-07-08 12:58:19 +0200

    ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico";*/
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+/*function Excuse (who, action, what, when) {
+  //for (let i = 0; i < 5; i ++) 
+  return (who + " " + action + " " + what + " " + when);  
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+console.log(Excuse);
+
+}*/
+
+function generadorExcusas() {
+
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  function posicionAleatoria(arrayLongitud) {
+
+    const NumberRandom = Math.floor(Math.random() * arrayLongitud.length);
+    return NumberRandom;
+  }
+
+  const posicionWho = posicionAleatoria(who);
+  const posicionAction = posicionAleatoria(action);
+  const posicionWhat = posicionAleatoria(what);
+  const posicionWhen = posicionAleatoria(when);
+
+  return (
+    who[posicionWho] + " " + action[posicionAction] + what[posicionWhat] + when[posicionWhen]
+  );
+
+}
+
+console.log(generadorExcusas());
+
+window.onload = function () {
+  document. getElementById("excuse").innerHTML=generadorExcusas()
+  console.log("Lo logre!");
 };
